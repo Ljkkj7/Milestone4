379ed705450e960114fab15e502c8d389dcd5423
ljkkj7 <kianjenkins26@gmail.com>
2025-12-01 12:13:26 +0000

    Add static files directory + start of css styling

--- sneakerhub/sneakerhub/urls.py
+++ sneakerhub/sneakerhub/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import homePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homePage.as_view(), name='home'),
 ]
