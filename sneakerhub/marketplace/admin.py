518e15eebe2fb6261343526edb57457d43d936f3
ljkkj7 <kianjenkins26@gmail.com>
2025-12-02 12:23:59 +0000

    Register of sneaker model in admin.py

--- sneakerhub/marketplace/admin.py
+++ sneakerhub/marketplace/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Sneaker)
+
+class SneakerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'size', 'price', 'owner', 'created_at')
+    search_fields = ('name', 'brand', 'owner__username')
