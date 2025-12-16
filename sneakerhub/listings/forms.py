cbbf805349b803e5ca3c7276df8c07037cd241db
ljkkj7 <kianjenkins26@gmail.com>
2025-12-16 10:22:38 +0000

    Refactored form to use Django & added CRUD views to listing apps to provide functionality to form

---
+++ sneakerhub/listings/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from marketplace.models import Sneaker
+
+
+class ListingCreationForm(forms.ModelForm):
+    class Meta:
+        model = Sneaker
+        fields = ['name', 'brand', 'size', 'price', 'description', 'image']
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if description and len(description) < 10:
+            raise forms.ValidationError("Description must be at least 10 characters long.")
+        return description
--- sneakerhub/listings/views.py
+++ sneakerhub/listings/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ListingCreationForm
 
-# Create your views here.
-def listingsPageView(request):
-    return render(request, 'create_listing.html')
+@login_required
+def createListingView(request):
+    """Handle creation of a new sneaker listing using a ModelForm."""
+    if request.method == 'POST':
+        form = ListingCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            sneaker = form.save(commit=False)
+            sneaker.owner = request.user
+            sneaker.save()
+            return render(request, 'profile.html', {'user_id': request.user.id})
+    else:
+        form = ListingCreationForm()
+
+    return render(request, 'create_listing.html', {'form': form})
