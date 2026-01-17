216f30198fe4f6e13c06f238388c7a1eb9b406a5
ljkkj7 <kianjenkins26@gmail.com>
2026-01-17 15:13:16 +0000

    initial HTML skeleton for public profile page

--- sneakerhub/publicprofile/views.py
+++ sneakerhub/publicprofile/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def publicProfileView(request, profile_user):
-    
     user = User.objects.get(id=profile_user)
-    return render(request, 'public_profile.html', {'profile_user': user})
+    sneakers = user.sneakers.filter(is_sold=False)
+    
+    return render(request, 'public_profile.html', {'profile_user': user, 'sneakers': sneakers})
