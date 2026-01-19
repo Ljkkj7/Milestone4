1ca4c7b055d9574495e410dea3189828e6e31de5
ljkkj7 <kianjenkins26@gmail.com>
2026-01-19 11:46:05 +0000

    Implemeneted retrieval and display of users previous listings

--- sneakerhub/publicprofile/views.py
+++ sneakerhub/publicprofile/views.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 def publicProfileView(request, profile_user):
+    
     user = User.objects.get(id=profile_user)
     sneakers = user.sneakers.filter(is_sold=False)
-    
-    return render(request, 'public_profile.html', {'profile_user': user, 'sneakers': sneakers})
+    sold_sneakers = user.sneakers.filter(is_sold=True).order_by('updated_at')
+
+    context = {
+        'profile_user': user,
+        'sneakers': sneakers,
+        'sold_sneakers': sold_sneakers,
+    }
+
+    return render(request, 'public_profile.html', context)
+
