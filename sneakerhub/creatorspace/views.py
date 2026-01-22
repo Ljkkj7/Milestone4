51c383275da7e2883ec6b402ac767d941a341850
ljkkj7 <kianjenkins26@gmail.com>
2026-01-22 14:47:45 +0000

    Initial HTML + Django for creator space landing page

--- sneakerhub/creatorspace/views.py
+++ sneakerhub/creatorspace/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Brand
 
 # Create your views here.
 def creatorSpaceView(request):
-    return render(request, 'creatorspace/creatorspace.html')
+    user = request.user
+
+    brands = Brand.objects.all()
+    isBrand = False
+
+    if user.id in brands.values_list('owner_id', flat=True):
+        isBrand = True
+
+    context = {
+        'brands': brands,
+        'isBrand': isBrand,
+    }
+
+    return render(request, 'creatorspace/creatorspace.html', context)
