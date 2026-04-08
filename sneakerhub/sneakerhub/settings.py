c96d010283e71bc41e0d5a4b1bfebb1ce0268346
ljkkj7 <kianjenkins26@gmail.com>
2026-04-08 20:46:56 +0100

    Updated allowed hosts for deployement

--- sneakerhub/sneakerhub/settings.py
+++ sneakerhub/sneakerhub/settings.py
@@ -35,7 +35,7 @@ STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sneakerhub.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
