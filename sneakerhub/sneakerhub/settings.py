a96ff7a71173b3c01475b04d66bed40b36ad1bc8
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-06-08 00:43:38 +0100

    Update settings.py

--- sneakerhub/sneakerhub/settings.py
+++ sneakerhub/sneakerhub/settings.py
@@ -154,7 +154,10 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
