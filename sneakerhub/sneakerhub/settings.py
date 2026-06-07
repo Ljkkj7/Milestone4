1140e7d68a8bd9dcf327d51c3bdc6acb9e45ba63
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-06-08 00:03:48 +0100

    Update settings.py

--- sneakerhub/sneakerhub/settings.py
+++ sneakerhub/sneakerhub/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
     'core',
     'marketplace',
     'account',
