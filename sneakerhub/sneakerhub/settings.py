b81180857e2a27dc74c010ab5b766eb190b3713e
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-06-08 00:52:52 +0100

    Update settings.py

--- sneakerhub/sneakerhub/settings.py
+++ sneakerhub/sneakerhub/settings.py
@@ -65,6 +65,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'STATICFILES': False
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
