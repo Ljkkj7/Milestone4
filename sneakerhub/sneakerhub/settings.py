5b8baea746cba6be5f9d6fbefe736ecefbad0ab1
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-06-08 01:12:03 +0100

    Update settings.py

--- sneakerhub/sneakerhub/settings.py
+++ sneakerhub/sneakerhub/settings.py
@@ -151,10 +151,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR.parent / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = BASE_DIR.parent / 'staticfiles'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
