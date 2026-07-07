d60122795bdf8b295488a571425ee972a6f1e4f8
Kian Jenkins <45104624+Ljkkj7@users.noreply.github.com>
2026-07-07 02:54:55 +0100

    Update views.py

--- sneakerhub/core/views.py
+++ sneakerhub/core/views.py
@@ -16,7 +16,12 @@ class CustomerUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.help_text = none
+            
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
